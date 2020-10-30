40b7f5084f49a9abbc06c1c0eecb99503411afc1
Osagie Omigie <osagieomigie@gmail.com>
2020-10-30 13:22:44 -0600

    Refactored tab depth, & inspectConstructor

--- Inspector.java
+++ Inspector.java
@@ -14,8 +14,7 @@ public class Inspector {
         inspectClass(c, obj, recursive, 0);
     }
     
-    private void inspectClass(Class<?> c, Object obj, boolean recursive, int depth) {
-    	// print declaring class
+    public StringBuilder calculateTabDepth(int depth) {
     	StringBuilder tmpTab = new StringBuilder("");
     	if (depth > 0) { 
 	    	for(int i = 0; i < depth; i++) {
@@ -23,8 +22,15 @@ public class Inspector {
 	    	}
     	}
     	
-    	System.out.println(tmpTab + "CLASS");
-    	System.out.println(tmpTab + String.format("Class: %s", c.getName()));
+    	return tmpTab;
+    }
+    
+    private void inspectClass(Class<?> c, Object obj, boolean recursive, int depth) {
+    	// print declaring class
+    	StringBuilder tabDepth = calculateTabDepth(depth);
+    	
+    	System.out.println(tabDepth + "CLASS");
+    	System.out.println(tabDepth + String.format("Class: %s", c.getName()));
     	
     	if (c.isArray()) {
     		inspectArray(c, obj, recursive, depth);
@@ -34,11 +40,11 @@ public class Inspector {
 	  
 	    	// print super class 
 	    	if(superClass != null) {
-	    			System.out.println(tmpTab + "SUPERCLASS -> Recursively Inspect");
-	    			System.out.println(tmpTab + String.format("SuperClass: %s", superClass.getName()));
+	    			System.out.println(tabDepth + "SUPERCLASS -> Recursively Inspect");
+	    			System.out.println(tabDepth + String.format("SuperClass: %s", superClass.getName()));
 	    			inspectClass(superClass, obj, recursive, depth+1);
 	    	}else {
-	    		System.out.println(tmpTab + "SuperClass: NONE");
+	    		System.out.println(tabDepth + "SuperClass: NONE");
 	    	}
 	    	
 	    	inspectInterfaces(c, obj, recursive, depth);
@@ -50,173 +56,160 @@ public class Inspector {
     
     private void inspectInterfaces(Class<?> c, Object obj, boolean recursive, int depth) {
     	// calculate depth 
-    	StringBuilder tmpTab = new StringBuilder("");
-    	if (depth > 0) { 
-	    	for(int i = 0; i < depth; i++) {
-	    		tmpTab.append("\t");
-	    	}
-    	}
+    	StringBuilder tabDepth = calculateTabDepth(depth);
     	
     	Class<?>[] interfaces = c.getInterfaces();
-    	System.out.println(tmpTab + String.format("INTERFACES( %s )", c.getName()));
+    	System.out.println(tabDepth + String.format("INTERFACES( %s )", c.getName()));
     	if (interfaces.length > 0) {
-    		System.out.println(tmpTab + "Interfaces-> ");
+    		System.out.println(tabDepth + "Interfaces-> ");
 	        for (Class<?> anInterface : interfaces) {
-	        	System.out.println(tmpTab + " INTERFACE -> Recursively Inspect");
-	            System.out.println(tmpTab +" " + anInterface.getName());
+	        	System.out.println(tabDepth + " INTERFACE -> Recursively Inspect");
+	            System.out.println(tabDepth +" " + anInterface.getName());
 	            // recursively recurse on interfaces 
 	            inspectClass(anInterface, obj, recursive, depth+1);
 	        }
     	}else {
-    		System.out.println(tmpTab + "Interfaces-> NONE");
+    		System.out.println(tabDepth + "Interfaces-> NONE");
     	}
     }
     
+    public void printExceptions(Class<?> ref [], StringBuilder tmpTab) {
+    	 
+		if (ref.length > 0) {
+			System.out.println(tmpTab + "  Exceptions-> ");
+			for (Class <?> tmpExp : ref) {
+				System.out.println(tmpTab +"  class " + tmpExp.getName());
+			}
+		}else {
+			System.out.println(tmpTab + "  Exceptions-> NONE");
+		}
+    }
+    
+    public void printParameterTypes(Class<?> ref[], StringBuilder tmpTab) {
+    	if (ref.length > 0) {
+        	System.out.println(tmpTab +"  Parameter types:");
+        	for(Class<?> parameter : ref) {
+        		if(parameter.isPrimitive())
+        			System.out.println(tmpTab + "   "+ parameter.getName());
+        		else
+        			System.out.println(tmpTab + "   class "+ parameter.getName());
+        	}
+        }else {
+        	System.out.println(tmpTab +"  Parameter types-> NONE");
+        }
+    }
+    
     private void inspectConstructors(Class<?> c, Object obj, boolean recursive, int depth) {
-    	// calculate depth 
-    	StringBuilder tmpTab = new StringBuilder("");
-    	if (depth > 0) { 
-	    	for(int i = 0; i < depth; i++) {
-	    		tmpTab.append("\t");
-	    	}
-    	}
+    	// calculate tab depth 
+    	StringBuilder tabDepth = calculateTabDepth(depth);
     	
     	// print constructors 
     	Constructor<?>[] constructors = c.getDeclaredConstructors();
-    	System.out.println(tmpTab + String.format("CONSTRUCTORS( %s )", c.getName()));
+    	System.out.println(tabDepth + String.format("CONSTRUCTORS( %s )", c.getName()));
     	if (constructors.length > 0) {
-    		System.out.println(tmpTab + "Constructors-> ");
+    		System.out.println(tabDepth + "Constructors-> ");
 	        for (Constructor<?> aConstructor : constructors) {
-	        	System.out.println(tmpTab + " CONSTRUCTOR");
+	        	System.out.println(tabDepth + " CONSTRUCTOR");
 	        	
 	        	// print name of constructor
-	            System.out.println(tmpTab +"  Name: " + aConstructor.getName());
+	            System.out.println(tabDepth +"  Name: " + aConstructor.getName());
 	            
 	            // print exceptions of constructor 
 	            Class<?>[] tmpExceptions = aConstructor.getExceptionTypes();
-	            if (tmpExceptions.length > 0) {
-	            	System.out.println(tmpTab +"  Exceptions->");
-	            	for(Class<?> exception : tmpExceptions) {
-	            		System.out.println(tmpTab +"  class " + exception.getName());
-	            	}
-	            }else {
-	            	System.out.println(tmpTab +"  Exceptions-> NONE");
-	            }
+	            printExceptions(tmpExceptions, tabDepth);
 	            
 	            // print parameter types of constructor 
 	            Class<?>[] tmpParameters = aConstructor.getParameterTypes();
-	            if (tmpParameters.length > 0) {
-	            	System.out.println(tmpTab +"  Parameter types:");
-	            	for(Class<?> parameter : tmpParameters) {
-	            		if(parameter.isPrimitive())
-	            			System.out.println(tmpTab + "   "+ parameter.getName());
-	            		else
-	            			System.out.println(tmpTab + "   class "+ parameter.getName());
-	            	}
-	            }else {
-	            	System.out.println(tmpTab +"  Parameter types-> NONE");
-	            }
+	            printParameterTypes(tmpParameters, tabDepth);
 	            
 	            // print modifiers of constructor 
 	            if ( Modifier.toString(aConstructor.getModifiers()).isEmpty()) {
-	            	System.out.println(tmpTab +"  Modifiers: NONE");
+	            	System.out.println(tabDepth +"  Modifiers: NONE");
 	            }else {
-	            	System.out.println(tmpTab + "  Modifiers: " + Modifier.toString(aConstructor.getModifiers()));
+	            	System.out.println(tabDepth + "  Modifiers: " + Modifier.toString(aConstructor.getModifiers()));
 	            }
-	            
 	        }
     	}else {
-    		System.out.println(tmpTab + "Constructors-> NONE");
+    		System.out.println(tabDepth + "Constructors-> NONE");
     	}
-    	
     }
     
     private void inspectMethods(Class<?> c, Object obj, boolean recursive, int depth) {
     	// calculate depth 
-    	StringBuilder tmpTab = new StringBuilder("");
-    	if (depth > 0) { 
-	    	for(int i = 0; i < depth; i++) {
-	    		tmpTab.append("\t");
-	    	}
-    	}
+    	StringBuilder tabDepth = calculateTabDepth(depth);
     	
-    	System.out.println(tmpTab + String.format("METHODS( %s )", c.getName()));
+    	System.out.println(tabDepth + String.format("METHODS( %s )", c.getName()));
     	
     	Method [] methods = c.getDeclaredMethods();
     	
     	if(methods.length > 0) {
-    		System.out.println(tmpTab + "Methods-> ");
+    		System.out.println(tabDepth + "Methods-> ");
 	    	for (Method m : methods) {
-	    		System.out.println(tmpTab + " METHOD");
+	    		System.out.println(tabDepth + " METHOD");
 	    		
 	    		// print method name
-	    		System.out.println(tmpTab + "  Name: " + m.getName());
+	    		System.out.println(tabDepth + "  Name: " + m.getName());
 	    		
 	    		// print method exceptions 
 	    		Class<?> [] exps = m.getExceptionTypes();
 	    		if (exps.length > 0) {
-	    			System.out.println(tmpTab + "  Exceptions-> ");
+	    			System.out.println(tabDepth + "  Exceptions-> ");
 	    			for (Class <?> tmpExp : exps) {
-	    				System.out.println(tmpTab +"  class " + tmpExp.getName());
+	    				System.out.println(tabDepth +"  class " + tmpExp.getName());
 	    			}
 	    		}else {
-	    			System.out.println(tmpTab + "  Exceptions-> NONE");
+	    			System.out.println(tabDepth + "  Exceptions-> NONE");
 	    		}
 	    		
 	    		// print parameter types of method 
 	            Class<?>[] tmpParameters = m.getParameterTypes();
 	            if (tmpParameters.length > 0) {
-	            	System.out.println(tmpTab +"  Parameter types:");
+	            	System.out.println(tabDepth +"  Parameter types:");
 	            	for(Class<?> parameter : tmpParameters) {
 	            		if(parameter.isPrimitive())
-	            			System.out.println(tmpTab + "  "+ parameter.getName());
+	            			System.out.println(tabDepth + "  "+ parameter.getName());
 	            		else
-	            			System.out.println(tmpTab + "  class "+ parameter.getName());
+	            			System.out.println(tabDepth + "  class "+ parameter.getName());
 	            	}
 	            }else {
-	            	System.out.println(tmpTab +"  Parameter types-> NONE");
+	            	System.out.println(tabDepth +"  Parameter types-> NONE");
 	            }
 	            
 	            // print method return type 
-	            System.out.println(tmpTab + "  Return type: " + m.getReturnType().getName());
+	            System.out.println(tabDepth + "  Return type: " + m.getReturnType().getName());
 	            
 	            // print modifiers of constructor 
 	            if ( Modifier.toString(m.getModifiers()).isEmpty()) {
-	            	System.out.println(tmpTab +"  Modifiers: NONE");
+	            	System.out.println(tabDepth +"  Modifiers: NONE");
 	            }else {
-	            	System.out.println(tmpTab + "  Modifiers: " + Modifier.toString(m.getModifiers()));
+	            	System.out.println(tabDepth + "  Modifiers: " + Modifier.toString(m.getModifiers()));
 	            }
 	    	}
     	}else {
-    		System.out.println(tmpTab + "Methods-> NONE");
+    		System.out.println(tabDepth + "Methods-> NONE");
     	}
 	    	
     }
     
     private void inspectFields(Class<?> c, Object obj, boolean recursive, int depth) {
     	// calculate depth 
-    	StringBuilder tmpTab = new StringBuilder("");
-    	if (depth > 0) { 
-	    	for(int i = 0; i < depth; i++) {
-	    		tmpTab.append("\t");
-	    	}
-    	}
+    	StringBuilder tabDepth = calculateTabDepth(depth);
     	
-    	System.out.println(tmpTab + String.format("FIELDS( %s )", c.getName()));
+    	System.out.println(tabDepth + String.format("FIELDS( %s )", c.getName()));
     	
     	Field [] field = c.getDeclaredFields();
     	
     	if(field.length > 0) {
-    		System.out.println(tmpTab + "Fields-> ");
+    		System.out.println(tabDepth + "Fields-> ");
     		
     		// inspect each field 
     		for (Field f : field) {
-    			System.out.println(tmpTab + " FIELD");
+    			System.out.println(tabDepth + " FIELD");
     			f.setAccessible(true);
     			inspectField(f, obj, recursive, depth);
     		}
     	}else
-    		System.out.println(tmpTab + "Fields-> NONE");
+    		System.out.println(tabDepth + "Fields-> NONE");
     }
     
     private Object getValue(Field f, Object obj) {
@@ -232,28 +225,20 @@ public class Inspector {
     
     private void inspectField(Field f, Object obj, boolean recursive, int depth) {
     	// calculate depth 
-    	StringBuilder tmpTab = new StringBuilder("");
-    	if (depth > 0) { 
-	    	for(int i = 0; i < depth; i++) {
-	    		tmpTab.append("\t");
-	    	}
-    	}
+    	StringBuilder tabDepth = calculateTabDepth(depth);
     	
     	// print field name
-    	System.out.println(tmpTab + "  Name: " + f.getName());
+    	System.out.println(tabDepth + "  Name: " + f.getName());
     	
     	// print field type
     	Class<?> type = f.getType();
-//    	if(!type.isPrimitive()) {
-//    		System.out.println(tmpTab + "  Type: " + type);
-//    	}else 
-    	System.out.println(tmpTab + "  Type: " + type);
+    	System.out.println(tabDepth + "  Type: " + type);
     	
     	// print field modifiers 
     	if ( Modifier.toString(f.getModifiers()).isEmpty()) {
-        	System.out.println(tmpTab +"  Modifiers: NONE");
+        	System.out.println(tabDepth +"  Modifiers: NONE");
         }else {
-        	System.out.println(tmpTab + "  Modifiers: " + Modifier.toString(f.getModifiers()));
+        	System.out.println(tabDepth + "  Modifiers: " + Modifier.toString(f.getModifiers()));
         }
     	
     	// determine fields type, and print current value(s)
@@ -261,9 +246,9 @@ public class Inspector {
     		// print fields value 
     		Object value = getValue(f, obj);
     		if(value != null) {
-    			System.out.println(tmpTab + "  Value: " + value); 
+    			System.out.println(tabDepth + "  Value: " + value); 
     		}else {
-    			System.out.println(tmpTab + "  Value: null"); 
+    			System.out.println(tabDepth + "  Value: null"); 
     		}
     	}else if (type.isArray()) {
     		// inspect array
@@ -272,15 +257,15 @@ public class Inspector {
     		// inspect object
     		Object value = getValue(f, obj);
     		if (value != null) {
-	    		System.out.println(tmpTab + "  Value (ref): " + value.getClass().getName() + "@" + Integer.toHexString(System.identityHashCode(value)) );
+	    		System.out.println(tabDepth + "  Value (ref): " + value.getClass().getName() + "@" + Integer.toHexString(System.identityHashCode(value)) );
 				
 				// inspect class if recursive true 
 				if (recursive) {
-					System.out.println(tmpTab + "    -> Recursively inspect");
+					System.out.println(tabDepth + "    -> Recursively inspect");
 					inspectClass(value.getClass(), value, recursive, depth+1);
 				}
     		}else {
-    			System.out.println(tmpTab + "  Value: null"); 
+    			System.out.println(tabDepth + "  Value: null"); 
     		}
     	}
     	
@@ -288,12 +273,7 @@ public class Inspector {
     
     private void inspectFieldArray(Field f, Object obj, boolean recursive, int depth) {
     	// calculate depth 
-    	StringBuilder tmpTab = new StringBuilder("");
-    	if (depth > 0) { 
-	    	for(int i = 0; i < depth; i++) {
-	    		tmpTab.append("\t");
-	    	}
-    	}
+    	StringBuilder tabDepth = calculateTabDepth(depth);
     	
     	// array object
     	Object array = getValue(f, obj);
@@ -304,41 +284,36 @@ public class Inspector {
     
     private void inspectArray(Class<?> c, Object obj, boolean recursive, int depth) {
     	// calculate depth 
-    	StringBuilder tmpTab = new StringBuilder("");
-    	if (depth > 0) { 
-	    	for(int i = 0; i < depth; i++) {
-	    		tmpTab.append("\t");
-	    	}
-    	}
+    	StringBuilder tabDepth = calculateTabDepth(depth);
     	
     	// print component type
-    	System.out.println(tmpTab + "  Component Type: " + c.getComponentType());
+    	System.out.println(tabDepth + "  Component Type: " + c.getComponentType());
     	
     	// print array length
     	int arrLength = Array.getLength(obj);
-    	System.out.println(tmpTab + "  Length: " + arrLength);
+    	System.out.println(tabDepth + "  Length: " + arrLength);
     	
     	// inspect array elements
-    	System.out.println(tmpTab + "  Entries-> ");
+    	System.out.println(tabDepth + "  Entries-> ");
     	for (int i = 0; i < arrLength; i++) {
     		Object arrValue = Array.get(obj, i);
     		if(arrValue != null) {
     			// primitive object
     			if (c.getComponentType().isPrimitive())
-    				System.out.println(tmpTab + "   Value: " + arrValue);
+    				System.out.println(tabDepth + "   Value: " + arrValue);
     			// class object
     			else {
-    				System.out.println(tmpTab + "   Value (ref): " + arrValue.getClass().getName() + "@" + Integer.toHexString(arrValue.hashCode()) );
+    				System.out.println(tabDepth + "   Value (ref): " + arrValue.getClass().getName() + "@" + Integer.toHexString(arrValue.hashCode()) );
     				
     				// inspect class if recursive true 
     				if (recursive) {
-    					System.out.println(tmpTab + "    -> Recursively inspect");
+    					System.out.println(tabDepth + "    -> Recursively inspect");
     					inspectClass(arrValue.getClass(), arrValue, recursive, depth+1);
     				}
     			}
     				
     		}else {
-    			System.out.println(tmpTab + "   Value: null"); 
+    			System.out.println(tabDepth + "   Value: null"); 
     		}
     		
     	}
