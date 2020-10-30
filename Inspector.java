ea5884eacddb2e7f8aaade9f38b43778ae2e1862
Osagie Omigie <osagieomigie@gmail.com>
2020-10-29 18:16:31 -0600

    Inspects array fields

--- Inspector.java
+++ Inspector.java
@@ -15,7 +15,6 @@ public class Inspector {
         inspectClass(c, obj, recursive, 0);
     }
     
-
     private void inspectClass(Class<?> c, Object obj, boolean recursive, int depth) {
     	// print declaring class
     	StringBuilder tmpTab = new StringBuilder("");
@@ -42,6 +41,7 @@ public class Inspector {
     	inspectInterfaces(c, obj, recursive, depth);
     	inspectConstructors(c, obj, recursive, depth);
     	inspectMethods(c, obj, recursive, depth);
+    	inspectFields(c, obj, recursive, depth);
     }
     
     private void inspectInterfaces(Class<?> c, Object obj, boolean recursive, int depth) {
@@ -188,5 +188,178 @@ public class Inspector {
     	}
 	    	
     }
-
+    
+    private void inspectFields(Class<?> c, Object obj, boolean recursive, int depth) {
+    	// calculate depth 
+    	StringBuilder tmpTab = new StringBuilder("");
+    	if (depth > 0) { 
+	    	for(int i = 0; i < depth; i++) {
+	    		tmpTab.append("\t");
+	    	}
+    	}
+    	
+    	System.out.println(tmpTab + String.format("FIELDS( %s )", c.getName()));
+    	
+    	Field [] field = c.getDeclaredFields();
+    	
+    	if(field.length > 0) {
+    		System.out.println(tmpTab + "Fields-> ");
+    		
+    		// inspect each field 
+    		for (Field f : field) {
+    			System.out.println(tmpTab + " FIELD");
+    			f.setAccessible(true);
+    			inspectField(f, obj, recursive, depth);
+    		}
+    	}else
+    		System.out.println(tmpTab + "Fields-> NONE");
+    }
+    
+    private Object getValue(Field f, Object obj) {
+    	try {
+    		return f.get(obj);
+    	}catch (IllegalArgumentException | NullPointerException | IllegalAccessException e){
+    		System.err.println("Can't access field " + obj);
+    	}
+    	
+    	return null;
+    }
+    
+    private void inspectField(Field f, Object obj, boolean recursive, int depth) {
+    	// calculate depth 
+    	StringBuilder tmpTab = new StringBuilder("");
+    	if (depth > 0) { 
+	    	for(int i = 0; i < depth; i++) {
+	    		tmpTab.append("\t");
+	    	}
+    	}
+    	
+    	// print field name
+    	System.out.println(tmpTab + "  Name: " + f.getName());
+    	
+    	// print field type
+    	Class<?> type = f.getType();
+//    	if(!type.isPrimitive()) {
+//    		System.out.println(tmpTab + "  Type: " + type);
+//    	}else 
+    	System.out.println(tmpTab + "  Type: " + type);
+    	
+    	// print field modifiers 
+    	if ( Modifier.toString(f.getModifiers()).isEmpty()) {
+        	System.out.println(tmpTab +"  Modifiers: NONE");
+        }else {
+        	System.out.println(tmpTab + "  Modifiers: " + Modifier.toString(f.getModifiers()));
+        }
+    	
+    	// determine fields type, and print current value(s)
+    	if(type.isPrimitive()) { 
+    		// print fields value 
+    		Object value = getValue(f, obj);
+    		if(value != null) {
+    			System.out.println(tmpTab + "  Value: " + value); 
+    		}else {
+    			System.out.println(tmpTab + "  Value: null"); 
+    		}
+    	}else if (type.isArray()) {
+    		// inspect array
+    		inspectFieldArray(f, obj, recursive, depth);
+    	}else { 
+    		// inspect object 
+    	}
+    	
+    }
+    
+    
+    private Object getArrayValue(Object obj, int index) {
+    	try {
+    		return Array.get(obj, index);
+    	}catch (IllegalArgumentException | NullPointerException | ArrayIndexOutOfBoundsException  e){
+    		System.err.println("Can't access array field " + obj);
+    	}
+    	
+    	return null;
+    }
+    
+    private void inspectFieldArray(Field f, Object obj, boolean recursive, int depth) {
+    	// array object
+    	Object array = getValue(f, obj);
+    	
+    	// inspect array
+    	inspectArray(f.getType(), array, recursive, depth);
+    	
+    	
+//    	// print component type
+//    	System.out.println(tmpTab + "  Component Type: " + f.getType().getComponentType());
+//    	
+//    	// print array length
+//    	int arrLength = Array.getLength(array);
+//    	System.out.println("  Length: " + arrLength);
+//    	
+//    	// inspect array elements
+//    	System.out.println("  Entries-> ");
+//    	for (int i = 0; i < arrLength; i++) {
+//    		Object arrValue = getArrayValue(array, i);
+//    		if(arrValue != null) {
+//    			// primitive object
+//    			if (arrValue.getClass().getComponentType().isPrimitive())
+//    				System.out.println(tmpTab + "   Value: " + arrValue);
+//    			// class object
+//    			else {
+//    				System.out.println(tmpTab + "   Value (ref): " + arrValue.getClass().getName() + Integer.toHexString(obj.hashCode()) );
+//    				
+//    				// inspect class if recursive true 
+//    				if (recursive) {
+//    					System.out.println("    -> Recursively inspect");
+//    					inspectClass(arrValue.getClass(), array, recursive, depth+1);
+//    				}
+//    			}
+//    				
+//    		}else {
+//    			System.out.println(tmpTab + "   Value: null"); 
+//    		}
+//    		
+//    	}
+    }
+    
+    private void inspectArray(Class<?> c, Object obj, boolean recursive, int depth) {
+    	// calculate depth 
+    	StringBuilder tmpTab = new StringBuilder("");
+    	if (depth > 0) { 
+	    	for(int i = 0; i < depth; i++) {
+	    		tmpTab.append("\t");
+	    	}
+    	}
+    	
+    	// print component type
+    	System.out.println(tmpTab + "  Component Type: " + c.getComponentType());
+    	
+    	// print array length
+    	int arrLength = Array.getLength(obj);
+    	System.out.println(tmpTab + "  Length: " + arrLength);
+    	
+    	// inspect array elements
+    	System.out.println(tmpTab + "  Entries-> ");
+    	for (int i = 0; i < arrLength; i++) {
+    		Object arrValue = getArrayValue(obj, i);
+    		if(arrValue != null) {
+    			// primitive object
+    			if (c.getComponentType().isPrimitive())
+    				System.out.println(tmpTab + "   Value: " + arrValue);
+    			// class object
+    			else {
+    				System.out.println(tmpTab + "   Value (ref): " + arrValue.getClass().getName() + "@" + Integer.toHexString(arrValue.hashCode()) );
+    				
+    				// inspect class if recursive true 
+    				if (recursive) {
+    					System.out.println(tmpTab + "    -> Recursively inspect");
+    					inspectClass(arrValue.getClass(), arrValue, recursive, depth+1);
+    				}
+    			}
+    				
+    		}else {
+    			System.out.println(tmpTab + "   Value: null"); 
+    		}
+    		
+    	}
+    }
 }
